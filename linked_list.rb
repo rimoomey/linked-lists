a3cb951e3f5aea71b1dd453f784da51d781a9b19
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-24 17:57:37 -0500

    Create class files.

---
+++ linked_list.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# Linked List implementation for ruby
+class LinkedList; end
---
+++ node.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# Standard single-pointer node class
+class Node; end
