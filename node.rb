10c4c2585094b2372bf3f9e35bc7c194d8491cae
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-24 17:59:49 -0500

    Add gemfile.

--- node.rb
+++ node.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 # Standard single-pointer node class
-class Node; end
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize
+    @value = nil
+    @next_node = nil
+  end
+end
