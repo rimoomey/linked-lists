1e3a328a9956f1907f656e61e745bbde0459b794
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-24 18:17:42 -0500

    Check append method for syntax errors.

--- linked_list.rb
+++ linked_list.rb
@@ -10,7 +10,18 @@ class LinkedList
     @size = 0
   end
 
-  def append(_value); end
+  def append(value)
+    new_tail = Node.new(value)
+    current_node = @head
+
+    current_node = current_node.next_node until current_node.next_node.nil?
+
+    current_node.next_node = new_tail
+    @tail = new_tail
+
+    @size += 1
+    nil
+  end
 
   def prepend(value)
     new_head = Node.new(value)
