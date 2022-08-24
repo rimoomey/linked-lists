5066f67f837128186aa37c087b3cc726c8a93c00
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-24 18:07:34 -0500

    Fix node initalize.

--- linked_list.rb
+++ linked_list.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
+require_relative 'node'
+
 # Linked List implementation for ruby
-class LinkedList; end
+class LinkedList
+  attr_reader :head, :tail, :size
+
+  def initialize
+    @size = 0
+  end
+
+  def prepend(value)
+    @head = Node.new.value = value if @size.zero?
+
+  end
+end
--- node.rb
+++ node.rb
@@ -4,8 +4,8 @@
 class Node
   attr_accessor :value, :next_node
 
-  def initialize
-    @value = nil
-    @next_node = nil
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
   end
 end
